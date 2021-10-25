b31bd84b65bd5b43c7d738c6f8e799125009d454
Yu-Muro <6u38e7c16rnhg5e37a0z@gmail.com>
2021-10-25 15:34:21 +0900

    データベース作成用コード

---
+++ pyapp/models/database.py
@@ -0,0 +1,48 @@
+from app import Base, engine
+from sqlalchemy import create_engine, Column, Integer, String, DateTime
+from sqlalchemy.orm import backref, relationship
+
+
+class UserTable(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, nullable=False, primary_key=True)
+    user_name = Column(String, nullable=False)
+    user_password = Column(String)
+
+
+class PurposeTable(Base):
+    __tablename__ = "purposes"
+    id = Column(Integer, nullable=False, primary_key=True)
+    date = Column(DateTime, default=datetime.datetime.now(pytz.timezone('Asia/Tokyo')))
+    user_id = Column(Integer, ForeignKey("UserTable.id"))
+    content = Column(String)
+    reserved_id = Column(Integer)
+    # テーブルの関連を定義
+    UserTable = relationship(
+        "UserTable", backref=backref("purposes", order_by=id))
+
+
+class ReserveTable(Base):
+    __tablename__ = "reserves"
+    id = Column(Integer, nullable=False, primary_key=True)
+    date = Column(DateTime, default=datetime.datetime.now(pytz.timezone('Asia/Tokyo')))
+    purpose_id = (Integer, ForeignKey("PurposeTable.id"))
+    # テーブルの関連を定義
+    PurposeTable = relationship(
+        "PurposeTable", backref=backref("reserves", order_by=id))
+
+
+class ChatTable(Base):
+    __tablename__ = "chats"
+    reserve_id = Column(Integer, ForeignKey("ReserveTable.id"))
+    date = Column(DateTime, default=datetime.datetime.now(pytz.timezone('Asia/Tokyo')))
+    content = Column(String)
+    user_id_sender = Column(Integer, ForeignKey("UserTable.id"))
+    # テーブルの関連を定義
+    ReserveTable = relationship(
+        "ReserveTable", backref=backref("chats", oder_by=id))
+    UserTable = relationship(
+        "UserTable", backref=backref("chats", order_by=id))
+
+def create_database():
+    Base.metadata.create_all(bind=engine, checkfirst=False)
